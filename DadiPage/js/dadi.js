cbd3a0a4187e93573c225160354c3b9b82dda4fc
JorgeLuix <tec.jorge.29@gmai.com>
2023-03-13 23:04:59 +0100

    giocchi dadi finita, modifica generale

---
+++ DadiPage/js/dadi.js
@@ -0,0 +1,43 @@
+
+
+let giocatoreScore = 0;
+let computerScore = 0;
+
+
+let giocatoreScoreDisplay = document.getElementById('giocatore-score');
+let computerScoreDisplay = document.getElementById('computer-score');
+let winnerDisplay = document.getElementById('winner');
+let Button = document.getElementById('btn-roll');
+let buttonReset = document.getElementById ('Reset');
+
+
+function rollDice() {
+
+  let giocatoreRoll = Math.floor(Math.random() * 6) + 1;
+  let computerRoll = Math.floor(Math.random() * 6) + 1;
+
+  giocatoreScore += giocatoreRoll;
+  computerScore += computerRoll;
+  giocatoreScoreDisplay.innerHTML = giocatoreScore;
+  computerScoreDisplay.innerHTML = computerScore;
+
+  
+  if (giocatoreScore > computerScore) {
+    winnerDisplay.innerHTML = 
+    `<p class="fw-bold text-success"> Hai vinto!!</p>`;
+  
+  } else if (computerScore > giocatoreScore) {
+    winnerDisplay.innerHTML = 
+    `<p class="fw-bold text-danger"> Hai perso!!</p>`;
+    
+  } else {
+    winnerDisplay.innerHTML = 
+    `<p class="fw-bold text-info-emphasis"> Hanno pareggiato!!</p>`;
+   
+  }
+}
+
+Button.addEventListener('click', rollDice);
+buttonReset.addEventListener('click', function() {
+    document.getElementById('giocatore-score').reset;
+});
